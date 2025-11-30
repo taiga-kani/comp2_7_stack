2bcc9d39e823f5d3d2b281260eece9ff85028ee2
Taiga Miura <taigamiura1623@gmail.com>
2025-11-30 22:54:29 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -14,13 +14,21 @@ void initialize(STACK* s, size_t mem_size)
 	s->stack_pointer = NULL;
 	s->stack_memory = NULL;
 	s->end = NULL;
+
+	s->stack_memory = (int*)malloc(mem_size);
+	s->stack_pointer = s->stack_memory;
+	s->end = s->stack_memory + (mem_size / sizeof(int));;
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
+	if (s == NULL) return;
 	// ToDo: Initializeで確保したメモリを解放しよう
+	s->stack_pointer = NULL;
+	s->stack_memory = NULL;
+	s->end = NULL;
 }
 
 
@@ -28,7 +36,15 @@ void finalize(STACK* s)
 bool push(STACK* s, int val)
 {
 	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL)return false;
+
+	if (s->stack_pointer >= s->end)return false;
+
+	*(s->stack_pointer) = val;
+	s->stack_pointer++;
+
+	return true;
+
 }
 
 
@@ -36,7 +52,17 @@ bool push(STACK* s, int val)
 bool push_array(STACK* s, int* addr, int num)
 {
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL)return false;
+	if (num <= 0) return false;
+	if (s->stack_pointer + num > s->end)return false;
+
+	for (int i = num - 1; i >= 0; i--)
+	{
+		*(s->stack_pointer) = addr[i];
+		s->stack_pointer++;
+	}
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
@@ -44,7 +70,12 @@ int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+	if (s == NULL)return 0;
+	if (s->stack_pointer <= s->stack_memory)  return 0;
+
+	s->stack_pointer--;
+	return *(s->stack_pointer);
+
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -53,5 +84,16 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL)return 0;
+	if (num <= 0) return false;
+	int count = 0;
+
+	while (count < num && s->stack_pointer > s->stack_memory)
+	{
+		s->stack_pointer--;
+		addr[count] = *(s->stack_pointer);
+		count++;
+	}
+	
+	return count;
 }
